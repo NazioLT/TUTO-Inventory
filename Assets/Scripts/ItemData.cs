86fde91677c92de8a9e9d1accd5bfe075f9cb959
Henno Théo <94365544+NazioLT@users.noreply.github.com>
2023-02-04 11:08:43 +0100

    episode 1

---
+++ Assets/Scripts/ItemData.cs
@@ -0,0 +1,8 @@
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "Item Data")]
+public class ItemData : ScriptableObject
+{
+    [SerializeField] public string itemName;
+    [SerializeField] public int stackMaxCount;
+}
